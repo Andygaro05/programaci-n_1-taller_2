9833696c88d59f8fbab5ff48c127846c22a402af
Andygaro05 <andrefelipelondono@gmail.com>
2024-06-08 08:27:47 -0500

    "Reconfiguración de Peleador.py y ajuste de main.py files, se añaden nuevas classes, metodos, sprites y variables, se ajustan algunas existentes y se implementa la logica de juego y elementos UI"

--- Peleador.py
+++ Peleador.py
@@ -1,13 +1,21 @@
+from typing import Any
 import pygame
-import time
+
+pygame.font.init()
+font = pygame.font.SysFont("Times New Roman", 24)
+negro = (0,0,0)
+rojo = (255,0,0)
+verde = (0,255,0)
+damage_text_group = pygame.sprite.Group()
 
 class Personaje:
-    def __init__(self, x, y, name, vida_max, fuerza, defensa):
+    def __init__(self, x, y, name, vida_max, fuerza, defensa, pociones):
         self.name = name
         self.vida_max = vida_max
         self.hp = vida_max
         self.fuerza = fuerza
         self.defensa = defensa
+        self.pociones = pociones
         self.inventario =[]
         self.vivo = True
         #obtenemos el tiempo
@@ -15,7 +23,7 @@ class Personaje:
         
         #creamos la lista que contendra los sprites para simular animación
         self.animation_list = []
-        self.action = 0 #0 idle, 1:atacar, 2:herido, 3:lastimado, 4:muerto
+        self.action = 0 #0 idle, 1:caminar, 2:atacar, 2:herido, 3:lastimado, 4:muerto
         self.frame_index = 0
 
         #cargar imagenes idle
@@ -64,62 +72,77 @@ class Personaje:
         self.animation_list.append(temp_list)
 
         self.image = self.animation_list[self.action][self.frame_index]
+
         #creamos la hitbox
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
+    def death(self):
+        self.action = 4
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+
+    def attack(self, objetivo):
+        #efectuar daño al objetivo
+        damage = self.fuerza - objetivo.defensa
+        objetivo.hp -= damage
+        damage_text = DamageText(objetivo.rect.centerx, objetivo.rect.y, str(damage), rojo)
+        damage_text_group.add(damage_text)
+        objetivo.lastimado()
+
+        #si el jugador muere
+        if objetivo.hp <1:
+            objetivo.hp = 0
+            objetivo.vivo = False
+
+        #configurar animación de ataque
+        self.action = 2
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+    def lastimado(self):
+        #configurar animación de daño
+        self.action = 3
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        
+    def idle(self):
+        #configurar animación idle
+        self.action = 0
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
     def update(self):
-        animation_cooldown = 100
-        #obtenemos la imagen acutal
+        animation_cooldown = 200
+            #obtenemos la imagen acutal
         self.image = self.animation_list[self.action][self.frame_index]
 
-        #si el tiempo entre el ultimo update y el cd de la animación es mayor muestra la siguiente imagen
+        #   si el tiempo entre el ultimo update y el cd de la animación es mayor muestra la siguiente imagen
         if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-            time.sleep(0.125)
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
-        #si la animación sobrepasa el tamaño del vector reinicia
-        if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            #si la animación sobrepasa el tamaño del vector reinicia
+            if self.frame_index >= len(self.animation_list[self.action]):
+                self.idle()
+            if self.hp < 1:
+                self.lastimado()
+                self.death()
+                #self.kill()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
-
-#clase base enemigo
-class Enemigo(Personaje):
-    def __init__(self, x, y, name, vida_max, fuerza, defensa):
-        super().__init__(x, y, name, vida_max, fuerza, defensa)
-
-        self.name = name
-        self.vida_max = vida_max
-        self.hp = vida_max
-        self.fuerza = fuerza
-        self.defensa = defensa
-        self.experiencia = 10  # Experiencia otorgada al derrotarlo
-        self.vivo = True    
-
-    def atacar(self, objetivo):
-        # Implementar lógica de ataque para enemigos
-        pass
-
-    def recibir_ataque(self, daño):
-        # Implementar lógica para recibir daño, considerando la defensa
-        pass
-
-    def soltar_recompensa(self):
-        # Implementar lógica para soltar recompensas aleatorias
-        pass
-
 #subclase ligero
-class Ligero(Enemigo):
+class Ligero(Personaje):
 
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, name, pociones):
         # Se heredan los valores iniciales del padre
-        super().__init__(x, y, name, vida_max=50, fuerza=15, defensa=5)
+        super().__init__(x, y, name, vida_max=100, fuerza=60, defensa=5, pociones=pociones)
         self.name = name
+        self.vida_max = self.vida_max
         self.hp = self.vida_max
-        self.inventario =[]
+        self.pociones = pociones
         self.vivo = True
 
         #obtenemos el tiempo
@@ -127,14 +150,14 @@ class Ligero(Enemigo):
         
         #creamos la lista que contendra los sprites para simular animación
         self.animation_list = []
-        self.action = 0 #0 idle, 1:atacar, 2:herido, 3:lastimado, 4:recuperación, 5:muerto
+        self.action = 0 #0 idle, 1:atacar, 2:herido, 3:recuperación, 4:muerto
         self.frame_index = 0
 
         #cargar imagenes idle
         temp_list = []  #almacenamos la acción que esta realizando
         for i in range(3):
             #insertamos la images
-            img = pygame.image.load(f"img/bandidos/Ligero/idle/LightBandit_Idle_{i}.png").convert_alpha()
+            img = pygame.image.load(f"img/bandidos/Ligero/Combat Idle/LightBandit_Combat Idle_{i}.png").convert_alpha()
             self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
             temp_list.append(self.image)
         self.animation_list.append(temp_list)
@@ -156,7 +179,7 @@ class Ligero(Enemigo):
             temp_list.append(self.image)  # Agregamos la imagen a la lista
         self.animation_list.append(temp_list)  # Agregamos la lista de imágenes a la lista principal de animaciones
 
-                #cargar imagenes recuperación
+        #cargar imagenes recuperación
         temp_list = []  #almacenamos la acción que esta realizando
         for i in range(7):
             #insertamos la images
@@ -164,7 +187,7 @@ class Ligero(Enemigo):
             self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
             temp_list.append(self.image)
         self.animation_list.append(temp_list)
-        
+
         #mostrar imagenes cuando muere
         temp_list = []  #almacenamos la acción que esta realizando
         img = pygame.image.load(f"img/bandidos/Ligero/Death/LightBandit_Death_{0}.png").convert_alpha()  # Cargamos solo la imagen en el índice 0
@@ -178,35 +201,74 @@ class Ligero(Enemigo):
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
+    def attack(self, objetivo):
+        #efectuar daño al objetivo
+        damage = self.fuerza - objetivo.defensa
+        objetivo.hp -= damage
+        damage_text = DamageText(objetivo.rect.centerx, objetivo.rect.y, str(damage), rojo)
+        damage_text_group.add(damage_text)
+        objetivo.lastimado()
+
+        #si el jugador muere
+        if objetivo.hp <1:
+            objetivo.hp = 0
+            objetivo.vivo = False
+
+        #configurar animación de ataque
+        self.action = 1
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        
+    def idle(self):
+        #configurar animación idle
+        self.action = 0
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+    def death(self):
+        #configurar animación de muerte
+        self.action = 4
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+    def lastimado(self):
+        #configurar animación de daño
+        self.action = 2
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
     def update(self):
-        animation_cooldown = 100
-        #obtenemos la imagen acutal
+        animation_cooldown = 200
+            #obtenemos la imagen acutal
         self.image = self.animation_list[self.action][self.frame_index]
 
-        #si el tiempo entre el ultimo update y el cd de la animación es mayor muestra la siguiente imagen
+        #   si el tiempo entre el ultimo update y el cd de la animación es mayor muestra la siguiente imagen
         if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-            time.sleep(0.125)
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
-        #si la animación sobrepasa el tamaño del vector reinicia
-        if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            #si la animación sobrepasa el tamaño del vector reinicia
+            if self.frame_index >= len(self.animation_list[self.action]):
+                self.idle()
+            if self.hp < 1:
+                self.lastimado()
+                self.death()
+                #self.kill()
+
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
     # Redefinir métodos si es necesario (ataque, recibir_ataque, etc.)
-
-
 #subclase pesado:
-class Pesado(Enemigo):
+class Pesado(Personaje):
 
-    def __init__(self, x, y, name):
+    def __init__(self, x, y, name, pociones):
         # Se heredan los valores iniciales del padre
-        super().__init__(x, y, name, vida_max=100, fuerza=10, defensa=20)
+        super().__init__(x, y, name, vida_max=300, fuerza=20, defensa=20, pociones=pociones)
         self.name = name
+        self.vida_max = self.vida_max
         self.hp = self.vida_max
-        self.inventario =[]
+        self.pociones = pociones
         self.vivo = True
 
         #obtenemos el tiempo
@@ -214,8 +276,17 @@ class Pesado(Enemigo):
         
         #creamos la lista que contendra los sprites para simular animación
         self.animation_list = []
-        self.action =  #0 idle, 1:atacar, 2:herido, 3:lastimado, 4:recuperación, 5:muerto
+        self.action = 0  #0 idle, 1:atacar, 2:herido, 3:lastimado, 4:recuperación, 5:muerto
         self.frame_index = 0
+
+        #cargar imagenes idle
+        temp_list = []  #almacenamos la acción que esta realizando
+        for i in range(3):
+            #insertamos la images
+            img = pygame.image.load(f"img/bandidos/Pesado/Combat idle/HeavyBandit_CombatIdle_{i}.png").convert_alpha()
+            self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
+            temp_list.append(self.image)
+        self.animation_list.append(temp_list)
         
         #cargar imagenes de ataque
         temp_list = []  #almacenamos la acción que esta realizando
@@ -226,29 +297,11 @@ class Pesado(Enemigo):
             temp_list.append(self.image)
         self.animation_list.append(temp_list)
 
-        #cargar imagenes cuando muere
-        temp_list = []  #almacenamos la acción que esta realizando
-        for i in range(1):
-            #insertamos la images
-            img = pygame.image.load(f"img/bandidos/Pesado/Death/HeavyBandit_Death_{i}.png").convert_alpha()
-            self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
-            temp_list.append(self.image)
-        self.animation_list.append(temp_list)
-
         #cuando lo lastiman
         temp_list = []  #almacenamos la acción que esta realizando
         for i in range(2):
             #insertamos la images
             img = pygame.image.load(f"img/bandidos/Pesado/Hurt/HeavyBandit_Hurt_{i}.png").convert_alpha()
-            self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
-            temp_list.append(self.image)
-        self.animation_list.append(temp_list)
-        
-        #cargar imagenes idle
-        temp_list = []  #almacenamos la acción que esta realizando
-        for i in range(3):
-            #insertamos la images
-            img = pygame.image.load(f"img/bandidos/Pesado/idle/HeavyBandit_Idle_{i}.png").convert_alpha()
             self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
             temp_list.append(self.image)
         self.animation_list.append(temp_list)
@@ -262,34 +315,146 @@ class Pesado(Enemigo):
             temp_list.append(self.image)
         self.animation_list.append(temp_list)
 
-        #mostrar imagenes cuando muere
+        #cargar imagenes cuando muere
         temp_list = []  #almacenamos la acción que esta realizando
-        img = pygame.image.load(f"img/bandidos/Pesado/Death/HeavyBandit_Death_{0}.png").convert_alpha()  # Cargamos solo la imagen en el índice 0
-        self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))  # Escalamos la imagen
-        temp_list.append(self.image)  # Agregamos la imagen a la lista
-        self.animation_list.append(temp_list)  # Agregamos la lista de imágenes a la lista principal de animaciones
+        for i in range(1):
+            #insertamos la images
+            img = pygame.image.load(f"img/bandidos/Pesado/Death/HeavyBandit_Death_{i}.png").convert_alpha()
+            self.image = pygame.transform.scale(img, (img.get_width()*6, img.get_height()*6))
+            temp_list.append(self.image)
+        self.animation_list.append(temp_list)
         self.image = self.animation_list[self.action][self.frame_index]
         
         #creamos la hitbox
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
+    def attack(self, objetivo):
+        #efectuar daño al objetivo
+        damage = self.fuerza - objetivo.defensa
+        objetivo.hp -= damage
+        damage_text = DamageText(objetivo.rect.centerx, objetivo.rect.y, str(damage), rojo)
+        damage_text_group.add(damage_text)
+        objetivo.lastimado()
+
+        #si el jugador muere
+        if objetivo.hp <1:
+            objetivo.hp = 0
+            objetivo.vivo = False
+            objetivo.death()
+        
+        
+        #configurar animación de ataque
+        self.action = 1
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        
+    def idle(self):
+        #configurar animación idle
+        self.action = 0
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+    def lastimado(self):
+        #configurar animación de daño
+        self.action = 2
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+
+    def death(self):
+        #configurar animación de muerte
+        self.action = 4
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        self.vivo = False
+
     def update(self):
-        animation_cooldown = 100
-        #obtenemos la imagen acutal
+        animation_cooldown = 200
+            #obtenemos la imagen acutal
         self.image = self.animation_list[self.action][self.frame_index]
 
-        #si el tiempo entre el ultimo update y el cd de la animación es mayor muestra la siguiente imagen
+        #   si el tiempo entre el ultimo update y el cd de la animación es mayor muestra la siguiente imagen
         if pygame.time.get_ticks() - self.update_time > animation_cooldown:
-            time.sleep(0.125)
             self.update_time = pygame.time.get_ticks()
             self.frame_index += 1
-        #si la animación sobrepasa el tamaño del vector reinicia
-        if self.frame_index >= len(self.animation_list[self.action]):
-            self.frame_index = 0
+            #si la animación sobrepasa el tamaño del vector reinicia
+            if self.frame_index >= len(self.animation_list[self.action]):
+                self.idle()
+            if self.hp < 1:
+                self.lastimado()
+                self.death()
+                #self.kill()
+
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
     # Redefinir métodos si es necesario (ataque, recibir_ataque, etc.)
+
+""""Creamos una clase con la barra de vida
+Seran 2 cuadrados uno rojo y verde, que cambiaran según
+el daño recibido"""
+
+class HealthBar():
+    def __init__(self, x, y, hp, max_hp):
+        self.x = x
+        self.y = y
+        self.hp = hp
+        self.max_hp = max_hp
+
+    def draw(self, screen, hp):
+        #actualizar con la salud actual
+        self.hp = hp
+        #controlar el tamaño del rectangulo verde
+        ratio = self.hp/self.max_hp
+        # Dibujar la barra de vida
+        pygame.draw.rect(screen, rojo, (self.x, self.y, 150, 20))#posición en X y Y
+        pygame.draw.rect(screen, verde, (self.x, self.y, 150*ratio, 20))
+
+
+""""Creamos una clase para crear los botones"""
+
+class Button():
+	def __init__(self, surface, x, y, image, size_x, size_y):
+		self.image = pygame.transform.scale(image, (size_x, size_y))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+		self.surface = surface
+
+	def draw(self):
+		action = False
+
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				action = True
+				self.clicked = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button
+		self.surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
+    
+class DamageText(pygame.sprite.Sprite):
+    def __init__(self, x, y, damage, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = font.render(str(damage), True, color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+        self.counter = 0
+
+    def update(self):
+        self.rect.y -= 1
+        #borrar el texto tras unos segundos
+        self.counter += 1
+        if self.counter > 30:
+            self.kill()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-from Peleador import Personaje  # Importar Personaje primero
-from Peleador import Enemigo, Ligero, Pesado
+from Peleador import *  # Importamos todo
+from Peleador import damage_text_group  #toca hacer el llamado individual
 
 pygame.init()
 
@@ -16,18 +16,40 @@ screen_heigt = 675 + panel_bajo
 screen = pygame.display.set_mode((screen_width, screen_heigt))
 pygame.display.set_caption("World of fantasy") #nombre del juego
 
+
+#variables de juego
+current_fighter = 1
+total_fighters = 3
+action_cooldown = 0
+action_wait_time = 90
+attack = False
+clicked = False
+pocion = False
+efecto_pocion = 20  #La pocion sana de a 20 de salud
+game_over = 0
+
+#fuente de juego
+font = pygame.font.SysFont("Times New Roman", 24)
+
+#definir colores
+negro = (0,0,0)
+rojo = (255,0,0)
+verde = (0,255,0)
+
 #cargamos las imagenes
-fondo_img = pygame.image.load("img/fondo/fondo resize.png").convert_alpha()#"img/fondo/fondo resize.png" 1200, 675
+fondo_img = pygame.image.load("img/fondo/fondo resize.png").convert_alpha()
 panel_img = pygame.image.load("img/iconos/boton madera.png").convert_alpha()
+pocion_img = pygame.image.load("img/iconos/pocion salud.png").convert_alpha()
+victoria =  pygame.image.load("img/iconos/victoria.png").convert_alpha()
+derrota = pygame.image.load("img/iconos/derrota.png").convert_alpha()
 
-#creamos la instancias de los personajes
-Caballero = Personaje(400, 560, "Caballero", 250, 50, 10)
-ligero1 = Ligero(800, 530, "Bandido Ligero")
-pesado1 = Pesado(1000, 530, "Pesado")
+#imagen de la espada para el mouse
+mouse_img = pygame.image.load("img/iconos/espada mouse.png").convert_alpha()
 
-bandidos = []
-bandidos.append(ligero1)
-bandidos.append(pesado1)
+#función para dibujar texto
+def dibujar_texto(texto, font, text_col, x, y):
+    img = font.render(texto, True, text_col)
+    screen.blit(img,(x,y))
 
 #para mostrar el fondo
 def dibujar_fondo():
@@ -36,6 +58,30 @@ def dibujar_fondo():
 def dibujar_panel():
     screen.blit(panel_img, (0, screen_heigt - panel_bajo))#el panel solo ocupa la parte baja de la pantalla
 
+    #mostrar stats del caballero
+    dibujar_texto(f"{Caballero.name} HP: {Caballero.hp}", font, negro, 100, screen_heigt - panel_bajo+ 10)
+
+    #mostrar stats bandidos
+    for count, i in enumerate(bandidos):
+        #mostrar nombre y vida
+        dibujar_texto(f"{i.name} HP: {i.hp}", font, negro, 800, (screen_heigt - panel_bajo+10)+ count * 60)
+
+#creamos la instancias de los personajes
+Caballero = Personaje(400, 560, "Caballero", 150, 50, 10,3)
+ligero1 = Ligero(800, 530, "Bandido Ligero",1)
+pesado1 = Pesado(1000, 530, "Enemigo Pesado",3)
+
+bandidos = []
+bandidos.append(ligero1)
+bandidos.append(pesado1)
+
+#Creamos las barras de vida
+barra_vida_caballero = HealthBar(100, screen_heigt-panel_bajo+40, Caballero.hp, Caballero.vida_max)
+barra_vida_ligero1 = HealthBar(800, screen_heigt-panel_bajo+40, ligero1.hp, ligero1.vida_max)
+barra_vida_pesado1 = HealthBar(800, screen_heigt-panel_bajo+100, pesado1.hp, pesado1.vida_max)
+
+#creamos los botones
+boton_pocion = Button(screen, 100, screen_heigt - panel_bajo + 70, pocion_img, 64, 64)
 
 #validamos que el juego se este ejecutando
 run = True
@@ -45,6 +91,9 @@ while run:
     #dibujar el fondo panel y caballero
     dibujar_fondo()
     dibujar_panel()
+    barra_vida_caballero.draw(screen, Caballero.hp)
+    barra_vida_ligero1.draw(screen, ligero1.hp)
+    barra_vida_pesado1.draw(screen, pesado1.hp)
 
     #dibujamos el estado actual del caballero
     Caballero.update()
@@ -55,10 +104,122 @@ while run:
         bandido.update()
         bandido.draw(screen)
 
+    #dibujar el daño
+    damage_text_group.update()
+    damage_text_group.draw(screen)
+
+    #Controlar las acciones del jugador
+    #reset las acciones
+    attack = False
+    objetivo = None
+    pocion = False
+    pos = pygame.mouse.get_pos()
+
+    #permitimos la visualización del mouse
+    pygame.mouse.set_visible(True)
+
+    #si tocamos la hitbox de un enemigo, cambiar el cursor a espada
+    for count, bandido in enumerate(bandidos):
+        if bandido.rect.collidepoint(pos):
+            #ocultamos el mouse
+            pygame.mouse.set_visible(False)
+            #mostramos la espada
+            screen.blit(mouse_img, pos)
+            #si se presiona el mouse atacamos al objetivo
+            if clicked == True and bandido.vivo == True:
+                attack = True
+                objetivo = bandidos[count]
+    if boton_pocion.draw():
+        pocion = True
+    #mostrar las pociones restantes
+    dibujar_texto(str(Caballero.pociones), font, rojo, 150, screen_heigt - panel_bajo + 110)
+
+    #validamos que el juego no haya finalizado
+    if game_over == 0:
+        #acción del jugador
+        if Caballero.vivo == True:
+            if current_fighter == 1:
+                action_cooldown += 1
+                if action_cooldown >= action_wait_time:
+                    #buscar acción de jugador
+                    #atacar
+                    if attack == True and objetivo != None:
+                        Caballero.attack(objetivo)
+                        current_fighter +=1
+                        action_cooldown = 0
+
+                    #pocion
+                    if pocion == True:
+                        if Caballero.pociones > 0:
+                            #validar para evitar overhealth
+                            if Caballero.vida_max - Caballero.hp > efecto_pocion:
+                                sanacion = efecto_pocion
+                            else:
+                                sanacion = Caballero.vida_max - Caballero.hp
+                            Caballero.hp += sanacion
+                            Caballero.pociones -= 1
+                            damage_text = DamageText(Caballero.rect.centerx, Caballero.rect.y, str(sanacion), verde)
+                            damage_text_group.add(damage_text)
+                            current_fighter += 1
+                            action_cooldown = 0
+        else:
+            game_over = -1
+
+        #acción del enemigo
+        #creamos el enumerate para recorrer bandidos y almacenar sus indices
+        for count,bandido in enumerate(bandidos):
+            if current_fighter == count + 2:#se inicia en 2 pq se arranca en 0 y 1 es el caballero
+                if bandido.vivo == True:
+                    action_cooldown += 1
+                    if action_cooldown >= action_wait_time:
+                        #validamos si necesita curarse primero
+                        if (bandido.hp / bandido.vida_max) < 0.5 and bandido.pociones >0:
+                            #validar que no haya overhealth
+                            if bandido.vida_max - bandido.hp > efecto_pocion:
+                                sanacion = efecto_pocion
+                            else:
+                                sanacion = bandido.vida_max - bandido.hp
+                            bandido.hp += sanacion
+                            bandido.pociones -= 1
+                            damage_text = DamageText(bandido.rect.centerx, bandido.rect.y, str(sanacion), verde)
+                            damage_text_group.add(damage_text)
+                            current_fighter += 1
+                            action_cooldown = 0
+                        else:    
+                            bandido.attack(Caballero)
+                            current_fighter += 1    #cambia de turno
+                            action_cooldown = 0 #reinicia el cd de cada acción
+                else:
+                    bandido.death()
+                    current_fighter += 1
+
+        #si todos los enemigos ya usaron su movida
+        if current_fighter > total_fighters:
+            current_fighter = 1
+    #validamos si game over
+    elif game_over != 0:
+        if game_over == 1:
+            screen.blit(victoria, (350, 400))
+        else:
+            screen.blit(derrota, (360, 250))
+
+    #validar si todos los bandidos estan muertos
+    bandidos_vivos = 0
+    for bandido in bandidos:
+        if bandido.vivo == True:
+            bandidos_vivos += 1
+
+    if bandidos_vivos == 0:
+        game_over = 1    
+
     #iteramos atraves de todos los eventos que ocurren
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            clicked = True
+        else:
+            clicked = False
 
     #actualizamos todas las acciones/funciones
     pygame.display.update()
